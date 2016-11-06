2f86b4f58daf237a32e63f554c36dd9dae3b3b98
Emily chen <Emily chen>
2016-11-06 09:30:15 +0800

    待解决：主页 字体 注册

--- js/index.js
+++ js/index.js
@@ -1,14 +1,16 @@
-function PreviewImage(inputFileEle) {
-	var filextension = inputFileEle.value.substring(inputFileEle.value.lastIndexOf("."), inputFileEle.value.length);
+;
+
+function validateImg(fileElement) {
+	var filextension = fileElement.value.substring(fileElement.value.lastIndexOf("."), fileElement.value.length);
 	filextension = filextension.toLowerCase();
 	if((filextension != '.jpg') && (filextension != '.gif') && (filextension != '.jpeg') && (filextension != '.png') && (filextension != '.bmp')) {
 		alert("对不起，系统仅支持标准格式的照片，请您调整格式后重新上传，谢谢 !");
-		inputFileEle.focus();
+		fileElement.focus();
 	} else {
 		var path;
 		if(document.all) //IE
 		{
-			inputFileEle.select();
+			fileElement.select();
 			path = document.selection.createRange().text;
 
 			document.getElementById("imgPreview").innerHTML = "";
@@ -16,25 +18,26 @@ function PreviewImage(inputFileEle) {
 		} else //FF
 		{
 			//						path = .getAsDataURL();
-			path = window.URL.createObjectURL(inputFileEle.files[0]);
-			inputFileEle.parentElement.previousElementSibling.innerHTML = "<img width='80px' height='80px' src='" + path + "'/>";
-			inputFileEle.parentElement.style.opacity = 0;
+			path = window.URL.createObjectURL(fileElement.files[0]);
+			fileElement.parentElement.previousElementSibling.innerHTML = "<img width='80px' height='80px' src='" + path + "'/>";
+			fileElement.parentElement.style.opacity = 0;
 			console.log(path);
 		}
 	}
-}
+};
 
 $(document).ready(function() {
-	$teacherInfor = $(".teacher-Info").eq(0);
+	var $teacherInfor = $(".teacher-Info").eq(0),
+		$uPanel = $(".userPanel").eq(0);
 
-	function switchPanel(role) {
-		if(typeof role === "string") {
-			if(role === "teacher") {
+	function switchPanel(strRole) {
+		if(typeof strRole === "string") {
+			if(strRole === "teacher") {
 				$(".teacher-Info").addClass("active");
 				$(".tutoring-Info").removeClass("active");
 				$("#teacher-Info").removeClass("hidden");
 				$("#tutoring-Info").addClass("hidden");
-			} else if(role === "tutor") {
+			} else if(strRole === "tutor") {
 				$(".teacher-Info").removeClass("active");
 				$(".tutoring-Info").addClass("active");
 				$("#teacher-Info").addClass("hidden");
@@ -45,29 +48,63 @@ $(document).ready(function() {
 		} else {
 			console.log("TypeError: Wrong Parameter;");
 		}
+	};
+
+	function uPanelHidden() {
+		$uPanel.addClass("hidden");
+		$(document).unbind('click', uPanelHidden);
 	}
-	//	switchPanel("me");
-	$(document).click(function(e) {
-		var element = e.target,
-			$target = $(element);
-		console.log(element);
-		//		alert(element);
 
-		//user manage
-		if($target.hasClass("fa-user")) {
-			$(".userPanel").toggleClass("hidden");
-			return;
-		} else {
-			$(".userPanel").addClass("hidden");
-		}
+	(function init() {
+		var $goIdentify = $(".go-identify").eq(0),
+			$uCenter = $(".user-center").eq(0),
+			$gotDetai = $(".go-tutorDetail").eq(0),
+			$goBack = $(".goBack").eq(0),
+			$teaInfo = $(".teacher-Info").eq(0),
+			$tuInfo = $(".tutoring-Info").eq(0),
+			$nav = $("nav").eq(0),
+			$moreInfo = $(".more-tutor-info").eq(0),
+			$reserve = $(".reserve").eq(0),
+			$subReserve = $(".submit-reserve");
 
-		if($target.hasClass("go-identify")) {
+		//交互优化
+		$("input").focus(function() {
+			this.select();
+		});
+
+
+		//必要事件绑定	
+		//用户页面    隐藏/显示 
+		$(document).click(function(e) {
+			var $target = $(e.target);
+			if($target.hasClass("fa-user")) {
+				$(".userPanel").toggleClass("hidden");
+				return;
+			} else {
+				$(".userPanel").addClass("hidden");
+				console.log(e.target);
+			}
+		});
+		//	$uPanel.click(function() {
+		//		alert("hei");
+		//		if($(this).hasClass("hidden")) {
+		//			alert("hei");
+		//			$(this).removeClass("hidden");
+		//			$(document).on('click', uPanelHidden);
+		//		}
+		//
+		//	});
+		
+		$goIdentify.click(function() {
 			window.location = "identify.html";
-		} else if($target.hasClass("my-home")){
-			window.location = "login.html"
-		}else if($target.hasClass("go-tutorDetail")) {
-			window.location = "tutorDetail.html"
-		} else if($target.hasClass("goBack")) {
+		});
+		$uCenter.click(function() {
+			window.location = "login.html";
+		});
+		$gotDetai.click(function() {
+			window.location = "tutorDetail.html";
+		});
+		$goBack.click(function() {
 			if($(".home-content").length === 0) {
 				history.go(-1);
 			} else {
@@ -75,29 +112,31 @@ $(document).ready(function() {
 				$("#tutoring-Info,nav").removeClass("hidden");
 				$("#tutoring-detail,.goBack").addClass("hidden");
 			}
-		}
-
+		});
 		/*----------------home page----------------------*/
-		//switch panel
-		if($target.hasClass("teacher-Info")) {
+		//			//switch panel
+		$teaInfo.click(function() {
 			switchPanel("teacher");
-			return;
-		} else if($target.hasClass("tutoring-Info")) {
+		});
+		$tuInfo.click(function() {
 			switchPanel("tutor");
-			return;
-		}
-		if($target.hasClass("more-tutor-info")) {
-			//			$.post(url,"more",function(){
+		});
+
+		$moreInfo.click(function() {
+
+			//			$.post(url,"moreInfo",function(){
 			//			
-			//			})
+			//			});
 			alert("没有更多信息，请随时关注我们的更新");
-			return;
-		}
+		});
+
 		/*---------------tutor Detail page----------------*/
-		if($target.hasClass("reserve")) {
+		$reserve.click(function() {
 			$("#tutoring-detail").addClass("hidden");
 			$("#reservation-Form").removeClass("hidden");
-		} else if($target.hasClass("submit-reserve")) {
+		});
+
+		$subReserve.click(function() {
 			var input = $("#reservation-Form  textarea").val();
 			if(input === "") {
 				alert("请您输入您的说明！")
@@ -113,16 +152,8 @@ $(document).ready(function() {
 				console.log(data);
 				alert("提交成功");
 			}
+		});
 
-		}
+	})();
 
-		/*---------------identify page----------------*/
-		//		if(){
-		//			
-		//			
-		//		}
-
-	
-	
-	})
 });
